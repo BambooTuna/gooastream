ff0a3de9e964be72644587497970d6216ff397a6
BambooTuna <bambootuna@gmail.com>
2021-08-16 22:40:04 +0900

    New: simple loggerを追加した

--- stream/logger.go
+++ stream/logger.go
@@ -1,5 +1,7 @@
 package stream
 
+import "fmt"
+
 var logger Logger
 
 func init() {
@@ -44,3 +46,35 @@ func (e emptyLoggerImpl) Errorf(format string, args ...interface{}) {
 }
 
 var _ Logger = (*emptyLoggerImpl)(nil)
+
+type simpleLoggerImpl struct{}
+
+func NewSimpleLoggerImpl() Logger {
+	return &simpleLoggerImpl{}
+}
+
+func (e simpleLoggerImpl) Tracef(format string, args ...interface{}) {
+	fmt.Printf("[Trace] "+format, args...)
+}
+
+func (e simpleLoggerImpl) Debugf(format string, args ...interface{}) {
+	fmt.Printf("[Debug] "+format, args...)
+}
+
+func (e simpleLoggerImpl) Printf(format string, args ...interface{}) {
+	fmt.Printf("[Print] "+format, args...)
+}
+
+func (e simpleLoggerImpl) Infof(format string, args ...interface{}) {
+	fmt.Printf("[Info] "+format, args...)
+}
+
+func (e simpleLoggerImpl) Warnf(format string, args ...interface{}) {
+	fmt.Printf("[Warn] "+format, args...)
+}
+
+func (e simpleLoggerImpl) Errorf(format string, args ...interface{}) {
+	fmt.Printf("[Error] "+format, args...)
+}
+
+var _ Logger = (*simpleLoggerImpl)(nil)
